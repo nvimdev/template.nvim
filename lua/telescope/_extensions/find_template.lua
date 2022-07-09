9fdcdeb78c6cb1fb886c62f4ed58a8f1c5bca50f
Raphael <glephunter@gmail.com>
2022-07-09 19:54:25 +0800

    format

--- lua/telescope/_extensions/find_template.lua
+++ lua/telescope/_extensions/find_template.lua
@@ -1,29 +1,29 @@
-local telescope = require("telescope")
-local finders = require("telescope.finders")
-local pickers = require("telescope.pickers")
-local make_entry = require("telescope.make_entry")
-local conf = require("telescope.config").values
+local telescope = require('telescope')
+local finders = require('telescope.finders')
+local pickers = require('telescope.pickers')
+local make_entry = require('telescope.make_entry')
+local conf = require('telescope.config').values
 
 local temp_list = function()
-	return require("template").get_all_temps()
+  return require('template').get_all_temps()
 end
 
 local find_template = function(opts)
-	opts = opts or {}
-	local results = temp_list()
+  opts = opts or {}
+  local results = temp_list()
 
-	pickers
-		.new(opts, {
-			prompt_title = "find in templates",
-			results_title = "templates",
-			finder = finders.new_table({
-				results = results,
-				entry_maker = make_entry.gen_from_file(opts),
-			}),
-			previewer = conf.file_previewer(opts),
-			sorter = conf.file_sorter(opts),
-		})
-		:find()
+  pickers
+    .new(opts, {
+      prompt_title = 'find in templates',
+      results_title = 'templates',
+      finder = finders.new_table({
+        results = results,
+        entry_maker = make_entry.gen_from_file(opts),
+      }),
+      previewer = conf.file_previewer(opts),
+      sorter = conf.file_sorter(opts),
+    })
+    :find()
 end
 
 return telescope.register_extension({ exports = { find_template = find_template } })
--- plugin/template.lua
+++ plugin/template.lua
@@ -1,22 +1,22 @@
 local api = vim.api
-local temp = require("template")
+local temp = require('template')
 
-api.nvim_create_user_command("Template", function(args)
-	require("template"):generate_template(args.args)
+api.nvim_create_user_command('Template', function(args)
+  require('template'):generate_template(args.args)
 end, {
-	nargs = "*",
-	complete = function(arg, line)
-		local cmd = vim.split(line, "%s+")
-		table.remove(cmd, 1)
-		local ft = vim.bo.filetype
+  nargs = '*',
+  complete = function(arg, line)
+    local cmd = vim.split(line, '%s+')
+    table.remove(cmd, 1)
+    local ft = vim.bo.filetype
 
-		if #cmd > 1 and cmd[1]:find("%.%w+$") then
-			ft = cmd[1]:match("[^.]+$")
-		end
+    if #cmd > 1 and cmd[1]:find('%.%w+$') then
+      ft = cmd[1]:match('[^.]+$')
+    end
 
-		local list = temp.get_temp_list()
-		return vim.tbl_filter(function(s)
-			return string.match(s, "^" .. arg)
-		end, list[ft])
-	end,
+    local list = temp.get_temp_list()
+    return vim.tbl_filter(function(s)
+      return string.match(s, '^' .. arg)
+    end, list[ft])
+  end,
 })
